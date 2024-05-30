6f366fc1b71f0b79179e2d24f8d2e01734c603eb
JerichoCabalan <jerichocabalan2@gmail.com>
2024-05-31 06:32:33 +0800

    fix

--- resources/js/components/views/private/PageUser/components/AllUsercomponents/TabsaAllcomponents/TabsRecentActivities.js
+++ resources/js/components/views/private/PageUser/components/AllUsercomponents/TabsaAllcomponents/TabsRecentActivities.js
@@ -34,6 +34,24 @@ export default function TabsRecentActivities() {
                         />
                     </Form.Item>
                 </Col>
+                <Col xs={10} sm={10} md={10} lg={10} xl={10} xxl={10}>
+                    <Form.Item name="payroll_code">
+                        <FloatSelect
+                            label="Edit Status"
+                            placeholder="Edit Status"
+                            options={[
+                                {
+                                    label: "Active",
+                                    value: "Active",
+                                },
+                                {
+                                    label: "Inactive",
+                                    value: "Inative",
+                                },
+                            ]}
+                        />
+                    </Form.Item>
+                </Col>
             </Form>
         </div>
     );
